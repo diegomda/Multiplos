f0a22c6dc435a048359b768732e08b0a41da43c3
K <K@DIEGO>
2017-09-08 23:53:05 -0300

    First commit

---
+++ Multiplos/Multiplos/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> lista1 = new List<int>();
+
+
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    lista1.Add(i);
+                }
+                if (i % 5 == 0)
+                {
+                    lista1.Add(i);
+                }
+            }
+
+            List<int> listadoFinal = lista1.Distinct().ToList();
+            foreach (var item in listadoFinal)
+            {
+                int suma = listadoFinal.Sum();
+                Console.WriteLine(suma);
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
